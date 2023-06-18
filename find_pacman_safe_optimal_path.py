e7b09a48c3acea34a5fd82fbb7483c8ec73e04ff
Sanskar Suryawanshi <sanskarsuryawanshi123@gmail.com>
2023-06-18 18:28:19 +0530

    added a nice table like reprsentation, and also a short logic to make sure that newly random coordinates won't be reapeated

--- find_pacman_safe_optimal_path.py
+++ find_pacman_safe_optimal_path.py
@@ -2,17 +2,29 @@ import random
 
 matrix = [[' ' for _ in range(8)] for _ in range(8)]
 
+marked_places = set()
+
 # Place 4 '*' at random row and column
 for _ in range(4):
     row = random.randint(0, 7)
     col = random.randint(0, 7)
-    matrix[row][col] = '*'
+    if (row, col) not in marked_places:
+        matrix[row][col] = '*'
+        marked_places.add((row, col))
 
 # Place 'P' at random row and column
-row = random.randint(0, 7)
-col = random.randint(0, 7)
-matrix[row][col] = 'P'
+while True:
+    row = random.randint(0, 7)
+    col = random.randint(0, 7)
+    if (row,col) not in marked_places:
+        matrix[row][col] = 'P'
+        marked_places.add((row, col))
+        break
 
 # Print the matrix
+print("\n+", "---+" * 8, sep="", end="")
 for row in matrix:
-    print(' '.join(row))
+    print("\n|", end="")
+    for ele in row:
+        print(f" {ele} |", end="")
+print("\n+", "---+" * 8, sep="")
