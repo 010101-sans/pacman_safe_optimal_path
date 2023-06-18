484f39675f7f432eb25bb62501409da2f9a8a0d6
Sanskar Suryawanshi <sanskarsuryawanshi123@gmail.com>
2023-06-18 18:34:56 +0530

    made it more functional, i found that there is some situation, where position are getting overlaped, i'll try to dig init

--- find_pacman_safe_optimal_path.py
+++ find_pacman_safe_optimal_path.py
@@ -1,30 +1,41 @@
-import random
+import random, os
 
-matrix = [[' ' for _ in range(8)] for _ in range(8)]
+matrix = None
 
-marked_places = set()
+def setup():
+    global matrix
+    
+    matrix = [[' ' for _ in range(8)] for _ in range(8)]
+    marked_places = set()
 
-# Place 4 '*' at random row and column
-for _ in range(4):
-    row = random.randint(0, 7)
-    col = random.randint(0, 7)
-    if (row, col) not in marked_places:
-        matrix[row][col] = '*'
-        marked_places.add((row, col))
+    # Place 4 '*' at random row and column
+    for _ in range(4):
+        row = random.randint(0, 7)
+        col = random.randint(0, 7)
+        if (row, col) not in marked_places:
+            matrix[row][col] = '*'
+            marked_places.add((row, col))
 
-# Place 'P' at random row and column
-while True:
-    row = random.randint(0, 7)
-    col = random.randint(0, 7)
-    if (row,col) not in marked_places:
-        matrix[row][col] = 'P'
-        marked_places.add((row, col))
-        break
+    # Place 'P' at random row and column
+    while True:
+        row = random.randint(0, 7)
+        col = random.randint(0, 7)
+        if (row,col) not in marked_places:
+            matrix[row][col] = 'P'
+            marked_places.add((row, col))
+            break
 
 # Print the matrix
-print("\n+", "---+" * 8, sep="", end="")
-for row in matrix:
-    print("\n|", end="")
-    for ele in row:
-        print(f" {ele} |", end="")
-print("\n+", "---+" * 8, sep="")
+def print_matrix():
+    global matrix
+    
+    print("\n+", "---+" * 8, sep="", end="")
+    for row in matrix:
+        print("\n|", end="")
+        for ele in row:
+            print(f" {ele} |", end="")
+    print("\n+", "---+" * 8, sep="")
+    
+os.system("cls")
+setup()
+print_matrix()
